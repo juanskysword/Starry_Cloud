bc7b1ba3770072fc7748af76d7c9515c6981bdca
Juan Tavera <juanskysword@gmail.com>
2022-02-15 16:10:40 -0800

    init

---
+++ Server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname, '../public/overworld.html'))
+})
+
+const port = process.env.PORT || 4000
+
+app.listen(port, () => console.log(`server running on ${port}`))
