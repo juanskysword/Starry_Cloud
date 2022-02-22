d1ca61ff74ee4ca9eeb079598cfdbbacf5fb00c3
juanskysword <juanskysword@gmail.com>
2022-02-22 15:38:59 -0600

    uploadHeroku1

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,6 @@ app.use(express.static('public'))
 app.get('/' , (req,res) => {
     res.sendFile(path.join(__dirname, '../public/overworld.html'))
 })
-
 const port = process.env.PORT || 25688
 
 app.listen(port, () => console.log(`server running on ${port}`))
